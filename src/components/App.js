ca0726d81f999fce7cf9382638d06d06c6a7bcff
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-10-08 09:33:26 -0600

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import 'normalize.css/normalize.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React, { Component } from 'react';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import { Grid, Row, Col } from 'react-bootstrap';
+
+class AppComponent extends Component {
+
+  componentWillMount() {
+    this.lock = new Auth0Lock('vubBKYrQzfjEIOefi61c2h1C3bAWrnQS', 'nicholasdorans.auth0.com');
+  }
+
+  render() {
+    return (
+      <div>
+        <Header lock={this.lock}></Header>
+        <Grid>
+          <Row>
+            <Col xs={12} md={3}>
+              <Sidebar />
+            </Col>
+            <Col xs={12} md={9}>
+              {this.props.children}
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default AppComponent;
